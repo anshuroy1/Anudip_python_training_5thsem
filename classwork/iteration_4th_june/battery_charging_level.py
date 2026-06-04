86a0a6e8f521c973f5a2056ff71f92cfdd01176e
Anshu Kumari <anshuroyanshuroy1@gmail.com>
2026-06-04 10:56:51 +0530

    classwork

---
+++ classwork/iteration_4th_june/battery_charging_level.py
@@ -0,0 +1,7 @@
+#program to displaying battery charing level
+charging_level = 20
+while(charging_level <= 100):
+    print("battery level:",charging_level,"%")
+    charging_level = charging_level+10
+ 
+print("battery full charged")

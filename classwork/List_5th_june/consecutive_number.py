fe2bf1ce9f1f92b0f21fecbe7e97dfeb262fabf1
Anshu Kumari <anshuroyanshuroy1@gmail.com>
2026-06-05 15:49:46 +0530

    consecutive number detecter

---
+++ classwork/List_5th_june/consecutive_number.py
@@ -0,0 +1,12 @@
+# List of numbers
+numbers = [4, 5, 6, 10, 11, 15, 16, 17]
+
+pairs = []
+
+# Check consecutive numbers
+for i in range(len(numbers) - 1):
+    if numbers[i] + 1 == numbers[i + 1]:
+        print(numbers[i], "and", numbers[i + 1], "are consecutive")
+        pairs.append((numbers[i], numbers[i + 1]))  # Store pair
+
+print("Consecutive Pairs:", pairs)
